0a37ac4ef8e76bec7c11bbbef2a06620361989fa
adi-mws <aditya040mth@gmail.com>
2025-03-30 13:48:03 +0530

    TailWind is initiated in the project and the project UI will be replaced by tailwind

--- server/db/connectDb.js
+++ server/db/connectDb.js
@@ -7,8 +7,7 @@ const connectDB = async () => {
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1);  // Exit the process with failure
+    process.exit(1);
   }
 };
-
 export default connectDB;
