8130f35809c8e0716611bb6c358daff33903c81a
adi-mws <aditya040mth@gmail.com>
2025-07-25 15:46:50 +0530

    Socket url fixation

--- client/src/hooks/ChatsContext.jsx
+++ client/src/hooks/ChatsContext.jsx
@@ -11,7 +11,7 @@ import axios from "axios";
 import { useNotification } from "./NotificationContext";
 import { useAuth } from "./AuthContext";
 
-export const socket = io(import.meta.env.VITE_SOCKET_URL || "http://localhost:3000", {
+export const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:3000", {
   autoConnect: false,
   withCredentials: true,
 });
