3867ec14f378dd20272d53454d433f34a0e7bb5c
adi-mws <aditya040mth@gmail.com>
2025-07-26 14:01:07 +0530

    Normal Account Login Fixed

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -95,7 +95,7 @@ export const loginUser = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,       // Prevent client-side access
       secure: process.env.NODE_ENV === 'production',  // Secure in production
-      sameSite: 'Strict',   // Prevent CSRF attacks
+      sameSite: 'None',   // Prevent CSRF attacks
       maxAge: 7 * 24 * 60 * 60 * 1000  // 7 days expiration
     });
 
